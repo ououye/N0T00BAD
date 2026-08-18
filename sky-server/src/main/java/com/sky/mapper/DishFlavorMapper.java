22fb1d3fb37713eb249a08e221756bde6d532895
itcyd <3045350744@qq.com>
2026-08-18 18:53:20 +0800

    feat: 后端notoobad外卖项目

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,33 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
+    void insertBatch(List<DishFlavor> flavors);
+
+
+    /**
+     * 根据菜品id删除
+     * @param dishid
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishid}")
+    void deleteByDishId(Long dishid);
+
+    /**
+     * 根据菜品id集合批量删除关联的口味数据
+     * @param dishId
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
+    void deleteByDishIds(List<Long> ids);
+}
